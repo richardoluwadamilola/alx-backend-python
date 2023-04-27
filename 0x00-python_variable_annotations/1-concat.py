15e39144a3e6d208746b58f2c42dcb59c5d61b28
Richard Oluwadamilola <oluwatobyrichard@gmail.com>
2023-04-27 13:32:07 +0100

    fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-"""Contains a single function that concatenates two strings."""
+'''task 1 concatenates two strings
+'''
+
 
 def concat(str1: str, str2: str) -> str:
-    """Concatenates two strings."""
+    ''' concatenates two string
+    '''
     return str1 + str2
